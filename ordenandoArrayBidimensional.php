ec5b8a448f76fb5c9ecea99d1d4ed8802ec83484
carlos-heins <carlosheinz053@gmail.com>
2024-02-22 20:38:04 -0300

    ordenando array bidimensional

---
+++ ordenandoArrayBidimensional.php
@@ -0,0 +1,36 @@
+<?php
+// Aqui temos um array desordenado
+$notas = [
+    [
+        'aluno' => 'Carlos',
+        'nota' => 7.5,
+    ],
+    [
+        'aluno' => 'Jessica',
+        'nota' => 8,
+    ],
+    [
+        'aluno' => 'Hayra',
+        'nota' => 8.9,
+    ],
+    [
+        'aluno' => 'Leandro',
+        'nota' => 2,
+    ]
+];
+
+// Aqui temos a função que vai ser passada para u usort
+function ordenaNotas(array $nota1, array $nota2)
+{
+    if ($nota1['nota'] > $nota2['nota']) {
+        return -1;
+    }
+    if ($nota1['nota'] < $nota2['nota']) {
+        return 1;
+    }
+    return 0;
+}
+// Aqui usamos usort que é uma forma de ordenar seguindo as minhas regras
+usort($notas, 'ordenaNotas');
+var_dump($notas);
+die;
